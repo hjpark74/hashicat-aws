eab174afa0d30ecf1a03c424a6af3db669f965ec
hjpark7459 <hjpark7459@gmail.com>
2023-03-17 05:25:52 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
 module "s3_bucket" {
   source = "app.terraform.io/hjpark7459-training/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
   bucket_prefix = "hjpark7459"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
 }
